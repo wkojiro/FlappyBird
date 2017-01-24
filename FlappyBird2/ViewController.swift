f75df7293063ed6f9a50ecc524dfff029d2c1117
Kojiro Wakabayashi <wkojiro@kojiro-no-macbook-pro.local>
2017-01-24 13:28:47 +0900

    Initial Commit

---
+++ FlappyBird2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlappyBird2
+//
+//  Created by Kojiro Wakabayashi on 2017/01/24.
+//  Copyright © 2017年 wkojiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
